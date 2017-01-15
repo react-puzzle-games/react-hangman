c98d055c76912590557814780880b24708157b64
ovidiubute <ovidiu.bute@gmail.com>
2017-01-15 11:01:33 +0200

    Render hangman character via props.

--- src/Hangman.js
+++ src/Hangman.js
@@ -1,16 +1,15 @@
-// @flow
-import React, { Component } from 'react';
+import React, { Component, PropTypes } from 'react';
 
 const styles = {
   character: {
     fill: 'none',
     stroke: 'black',
-    'strokeWidth': 3,
+    strokeWidth: 3,
   },
   post: {
     fill: 'none',
     stroke: 'black',
-    'strokeWidth': 10,
+    strokeWidth: 10,
   },
 };
 
@@ -20,15 +19,61 @@ class Hangman extends Component {
       <svg width="500" height="550">
         <polyline points="400,500 100,500 150,500 150,100 320,100 320,150"
                   style={styles.post} />
-        <line x1="320" y1="190" x2="320" y2="360" style={styles.character} />
-        <line x1="320" y1="260" x2="250" y2="230" style={styles.character} />
-        <line x1="320" y1="260" x2="390" y2="230" style={styles.character} />
-        <line x1="320" y1="360" x2="250" y2="450" style={styles.character} />
-        <line x1="320" y1="360" x2="390" y2="450" style={styles.character} />
+        {this._renderBody()}
+        {this._renderLeftArm()}
+        {this._renderRightArm()}
+        {this._renderLeftLeg()}
+        {this._renderRightLeg()}
         <circle cx="320" cy="190" r="40" fill="black" />
       </svg>
     );
   }
+
+  _renderBody() {
+    return this.props.body
+      ? <line x1="320" y1="190" x2="320" y2="360" style={styles.character} />
+      : null;
+  }
+
+  _renderLeftArm() {
+    return this.props.leftArm
+      ? <line x1="320" y1="260" x2="250" y2="230" style={styles.character} />
+      : null;
+  }
+
+  _renderRightArm() {
+    return this.props.rightArm
+      ? <line x1="320" y1="260" x2="390" y2="230" style={styles.character} />
+      : null;
+  }
+
+  _renderLeftLeg() {
+    return this.props.leftLeg
+      ? <line x1="320" y1="360" x2="250" y2="450" style={styles.character} />
+      : null;
+  }
+
+  _renderRightLeg() {
+    return this.props.rightLeg
+      ? <line x1="320" y1="360" x2="390" y2="450" style={styles.character} />
+      : null;
+  }
 }
+
+Hangman.propTypes = {
+  body: PropTypes.bool,
+  leftArm: PropTypes.bool,
+  rightArm: PropTypes.bool,
+  leftLeg: PropTypes.bool,
+  rightLeg: PropTypes.bool,
+};
+
+Hangman.defaultProps = {
+  body: true,
+  leftArm: true,
+  rightArm: true,
+  leftLeg: true,
+  rightLeg: true,
+};
 
 export default Hangman;
