28bdae39e0eb725637a854f00b1434e5708d19f6
ovidiubute <ovidiu.bute@gmail.com>
2017-01-26 23:02:33 +0200

    Add link to Github.

--- src/AppFooter.js
+++ src/AppFooter.js
@@ -6,10 +6,17 @@ class AppFooter extends Component {
   render() {
     return (
       <div className="App-footer">
+        <a target="_blank"
+          href="https://github.com/ovidiubute/react-hangman"
+        >
+          <img src="github-10-xxl.png" width="30" height="30"
+            alt="Github"
+          />
+        </a>
         <h5>Made with <span style={{color: 'red'}}>&#9829;</span> by <a
             target="_blank"
             href="https://twitter.com/ovidiubute">Ovidiu
-          </a> using React and create-react-app boilerplate &#169; 2017
+          </a> using React and create-react-app boilerplate
         </h5>
       </div>
     );
