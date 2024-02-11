398b7e00ae67337fd39a60e2e5eecd07c51875d0
Ovidiu Bute <ovidiu@fabricdata.com>
2024-02-11 17:58:50 +0200

    Bring project up to date

--- src/AppHeader.js
+++ src/AppHeader.js
@@ -1,13 +1,11 @@
 import React, { Component } from 'react';
 
 import './AppHeader.css';
-import logo from './logo.svg';
 
 class AppHeader extends Component {
   render() {
     return (
       <div className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <h2>React Hangman</h2>
       </div>
     );
